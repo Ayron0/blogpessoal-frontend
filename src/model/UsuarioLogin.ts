c89518e2fdb025a647ba5197990d135693c60163
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-27 13:42:26 -0300

    chore(package): add biblioteca Axios

--- src/model/UsuarioLogin.ts
+++ src/model/UsuarioLogin.ts
@@ -12,3 +12,4 @@ export default interface UsuarioLogin {
 // Entretanto, ao efetuar login na API, o objeto retornado pelo Backend inclui todos os dados do usuário, além do Token JWT.
 
 // Por esse motivo, a Model UsuarioLogin no Frontend foi construída para incluir todos esses atributos, permitindo que a aplicação manipule corretamente as informações recebidas.
+
