09e5f08cf5838efebe400634422e07d1e259b81e
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-28 21:25:34 -0300

    feat(listatemas): implementar a Lógica do Componente ListaTemas

--- src/components/tema/listatemas/ListaTemas.tsx
+++ src/components/tema/listatemas/ListaTemas.tsx
@@ -1,6 +1,46 @@
+import { useEffect, useState, useContext } from "react";
 import CardTema from "../cardtema/CardTema"
+import { buscar } from "../../../services/Service";
+import { useNavigate } from "react-router-dom";
+import type Tema from "../../../model/Tema";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 function ListaTemas() {
+
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [temas, setTemas] = useState<Tema[]>([])
+
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token
+
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado")
+      navigate("/")
+    }
+  }, [token])
+  
+  useEffect(() => {
+      buscarTemas()
+  },[temas.length])
+
+  async function buscarTemas(){
+    try {
+      setIsLoading(true)
+
+      await buscar('/temas', setTemas, {
+        headers: { Authorization: token }
+      })
+    } catch (error: any) {
+        if (error.toString().includes("401")) {
+          handleLogout()
+        }
+    } finally {
+        setIsLoading(false)
+    }
+  }
+
   return (
     <>
       <div className="flex justify-center w-full my-4">
