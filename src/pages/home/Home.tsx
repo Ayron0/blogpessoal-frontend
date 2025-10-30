81ffa77eb5ee20f62bf2a80d1325c7835d58d6c9
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-30 11:23:39 -0300

    feat(home): atualizar componente Home, importando o componente ListaPostagens

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+
 function Home() {
   return (
     <>
@@ -29,6 +31,8 @@ function Home() {
           </div>
         </div>
       </div>
+
+       <ListaPostagens />
     </>    
   )
 }
