fb7c657fc2f2b7da312c8753fba98ec444530dfd
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-30 08:06:53 -0300

    feat(deletartema): add o titulo e subtitulo do componente

--- src/components/tema/deletartema/DeletarTema.tsx
+++ src/components/tema/deletartema/DeletarTema.tsx
@@ -69,35 +69,42 @@ function DeletarTema() {
   }
 // border flex flex-col rounded-2xl overflow-hidden justify-between w-90
   return (
-    <div className="border rounded-2xl overflow-hidden container w-1/3 m-auto mt-4">
-      <header className="py-2 px-6 bg-indigo-800  text-white font-bold text-2xl">Tema</header>
-      <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
+    <div className="container w-1/3 mx-auto">
+      <h1 className="text-4xl text-center my-4">Deletar Tema</h1>
+      <p className="text-center font-semibold mb-4">
+        Você tem certeza de que deseja apagar o tema a seguir?
+      </p>
+      <div className="border rounded-2xl overflow-hidden  mt-4">
+        <header className="py-2 px-6 bg-indigo-800  text-white font-bold text-2xl">Tema</header>
+        <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
 
-      <div className="flex">
-        <button className="w-full text-slate-100 bg-red-400 hover:bg-red-700 flex items-center justify-center py-2"
-          onClick={retornar}
-        >
-          Não
-        </button>
+        <div className="flex">
+          <button className="w-full text-slate-100 bg-red-400 hover:bg-red-700 flex items-center justify-center py-2"
+            onClick={retornar}
+          >
+            Não
+          </button>
 
-        <button className=" w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800
-          flex items-center justify-center"
-          onClick={deletarTema}
+          <button className=" w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800
+            flex items-center justify-center"
+            onClick={deletarTema}
+            
+          >
+            {isLoading ? 
+              <ClipLoader 
+                color="#ffffff" 
+                size={20} 
+              /> :
+              <span>Sim</span>
+            }
+          </button>
           
-        >
-          {isLoading ? 
-            <ClipLoader 
-              color="#ffffff" 
-              size={20} 
-            /> :
-            <span>Sim</span>
-          }
-        </button>
-        
+
+        </div>
 
       </div>
-
     </div>
+    
   )
 }
 
