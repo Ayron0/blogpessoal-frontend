b2c2c69a1123167324f6e8a935716b4efb5492d6
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-31 18:48:54 -0300

    feat(navbar): implementa renderização condicional de visualizar o componente Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -6,7 +6,7 @@ function Navbar() {
 
     const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
         handleLogout()
@@ -14,8 +14,10 @@ function Navbar() {
         navigate('/')
     }
 
-    return (
-        <>
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+        component = (
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
@@ -35,6 +37,12 @@ function Navbar() {
                     </div>
                 </div>
             </div>
+        )
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
