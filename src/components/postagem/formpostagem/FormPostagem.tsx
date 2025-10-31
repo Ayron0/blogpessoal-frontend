438bfe182a55496b661e66f6a82d78967f2d3fb2
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-31 12:51:10 -0300

    feat: cria componente DeletarPostagem

--- src/components/postagem/formpostagem/FormPostagem.tsx
+++ src/components/postagem/formpostagem/FormPostagem.tsx
@@ -1,36 +1,41 @@
-import { useState, useEffect, useContext, type ChangeEvent, type FormEvent } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  useContext,
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type FormEvent,
+} from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { buscar, atualizar, cadastrar } from "../../../services/Service";
 import type Postagem from "../../../model/Postagem";
-import { ClipLoader } from 'react-spinners';
 import type Tema from "../../../model/Tema";
-
+import { atualizar, buscar, cadastrar } from "../../../services/Service";
 
 function FormPostagem() {
-
   const navigate = useNavigate();
-  const[isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const [temas, setTemas] = useState<Tema[]>([])
+  const [temas, setTemas] = useState<Tema[]>([]);
 
-  const [tema, setTema] = useState<Tema>({ id: 0, descricao: '',})
+  const [tema, setTema] = useState<Tema>({ id: 0, descricao: "" });
 
-  const [postagem, setPostagem] = useState<Postagem>({} as Postagem)
+  const [postagem, setPostagem] = useState<Postagem>({} as Postagem);
 
-  const {usuario, handleLogout} = useContext(AuthContext)
-  const token = usuario.token
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
 
-  const {id} = useParams<{id: string}>();
+  const { id } = useParams<{ id: string }>();
 
   async function buscarPostagemPorId(id: string) {
     try {
       await buscar(`/postagens/${id}`, setPostagem, {
-        headers: { Authorization: token} 
-      })
+        headers: { Authorization: token },
+      });
     } catch (error: any) {
-      if (error.toString().includes('401')) {
-        handleLogout()
+      if (error.toString().includes("401")) {
+        handleLogout();
       }
     }
   }
@@ -38,142 +43,146 @@ function FormPostagem() {
   async function buscarTemaPorId(id: string) {
     try {
       await buscar(`/temas/${id}`, setTema, {
-        headers: { Authorization: token} 
-      })
+        headers: { Authorization: token },
+      });
     } catch (error: any) {
-      if (error.toString().includes('401')) {
-        handleLogout()
+      if (error.toString().includes("401")) {
+        handleLogout();
       }
     }
   }
 
   async function buscarTemas() {
-      try {
-        await buscar('/temas', setTemas, {
-          headers: { Authorization: token} 
-        })
-      } catch (error: any) {
-        if (error.toString().includes('401')) {
-          handleLogout()
-        }
+    try {
+      await buscar("/temas", setTemas, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
       }
     }
+  }
 
   useEffect(() => {
-      if (token === "") {
-        alert("Você precisa estar logado")
-        navigate("/")
-      }
-    }, [token])
-    
-  useEffect(() => {
-    buscarTemas()
-    if(id !== undefined) {
-      buscarPostagemPorId(id)
+    if (token === "") {
+      alert("Você precisa estar logado");
+      navigate("/");
     }
-  },[id])
+  }, [token]);
+
+  useEffect(() => {
+    buscarTemas();
+    if (id !== undefined) {
+      buscarPostagemPorId(id);
+    }
+  }, [id]);
 
   useEffect(() => {
     setPostagem({
       ...postagem,
       tema: tema,
-    })
-  }, [tema])
+    });
+  }, [tema]);
 
-  function atualizarEstado(e: ChangeEvent<HTMLInputElement>){
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setPostagem({
       ...postagem,
       [e.target.name]: e.target.value,
       tema: tema,
       usuario: usuario,
     });
-  }
+  }// Essa parte do código é sobre o relacionamento das tabelas
 
   function retornar() {
-    navigate("/postagens")
+    navigate("/postagens");
   }
 
+  // FormEvent<HTMLFormElement> é um evento de formulario que vai mandar as informações para o back-end
   async function gerarNovaPostagem(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    setIsLoading(true)
+    e.preventDefault();
+    setIsLoading(true);
 
-    if (id  !== undefined) {
+    if (id !== undefined) {
       try {
         await atualizar(`/postagens`, postagem, setPostagem, {
-          headers: { 'Authorization': token },
+          headers: { Authorization: token },
         });
 
-        alert('Postagem atualizada com sucesso!')
+        alert("Postagem atualizada com sucesso!");
       } catch (error: any) {
-        if (error.toString().includes('401')) {
+        if (error.toString().includes("401")) {
           handleLogout();
         } else {
-          alert('Erro ao atualizar a Postagem.')
+          alert("Erro ao atualizar a Postagem.");
         }
       }
-
     } else {
       try {
         await cadastrar(`/postagens`, postagem, setPostagem, {
-          headers: { 'Authorization': token },
+          headers: { Authorization: token },
         });
 
-        alert('Postagem cadastrada com sucesso!')   
+        alert("Postagem cadastrada com sucesso!");
       } catch (error: any) {
-        if (error.toString().includes('401')) {
+        if (error.toString().includes("401")) {
           handleLogout();
         } else {
-          alert('Erro ao atualizar a Postagem.')
+          alert("Erro ao atualizar a Postagem.");
         }
       }
     }
 
-    setIsLoading(false)
-    retornar()
+    setIsLoading(false);
+    retornar();
   }
 
-  const carregandoTema = tema.descricao === '';
+  const carregandoTema = tema.descricao === "";
 
   return (
     <div className="container flex flex-col items-center mx-auto">
       <h1 className="text-4xl text-center my-8">
-        {id === undefined ? 'Editar Postagem' : 'Cadastrar Postagem'}
+        {id === undefined ? "Editar Postagem" : "Cadastrar Postagem"}
       </h1>
-      <form className="w-1/2  flex flex-col gap-4"
-        onSubmit={gerarNovaPostagem}>
+      <form className="w-1/2  flex flex-col gap-4" onSubmit={gerarNovaPostagem}>
         <div className=" flex flex-col gap-2">
-          <label htmlFor="titulo" >Título da Postagem</label>
-          <input 
+          <label htmlFor="titulo">Título da Postagem</label>
+          <input
             type="text"
             placeholder="Titulo"
             name="titulo"
             required
             className="border-2 border-slate-700 rounded p-2"
             value={postagem.titulo}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} 
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
         </div>
 
         <div className=" flex flex-col gap-2">
-          <label htmlFor="titulo" >Texto da Postagem</label>
-          <input 
+          <label htmlFor="titulo">Texto da Postagem</label>
+          <input
             type="text"
             placeholder="Texto"
             name="texto"
             required
             className="border-2 border-slate-700 rounded p-2"
             value={postagem.texto}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} 
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
         </div>
 
         <div className=" flex flex-col gap-2">
           <p>Tema da Postagem</p>
-          <select name="tema" id="tema" className="border-2 border-slate-800 rounded"
-            onChange={(e) => buscarTemaPorId(e.currentTarget.value)}
-          >
-            <option value="" selected disabled>Selecione um Tema</option>
-            {temas.map((tema) =>(
+          <select
+            name="tema"
+            id="tema"
+            className="border-2 border-slate-800 rounded"
+            onChange={(e) => buscarTemaPorId(e.currentTarget.value)} 
+          > {/*e.currentTarget.value - ele vai pegar apenas o valo/opção que foi clicado*/}
+            <option value="" selected disabled>
+              Selecione um Tema
+            </option>
+            {temas.map((tema) => (
               <>
                 <option value={tema.id}>{tema.descricao}</option>
               </>
@@ -181,24 +190,21 @@ function FormPostagem() {
           </select>
         </div>
 
-        <button 
+        <button
           className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white 
               font-bold w-1/2 py-2 mx-auto flex justify-center"
-            type="submit"
-            disabled={carregandoTema}
+          type="submit"
+          disabled={carregandoTema}
         >
-          {isLoading ? 
-            <ClipLoader 
-              color="#ffffff" 
-              size={20} 
-            /> :
-            <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
-          }
+          {isLoading ? (
+            <ClipLoader color="#ffffff" size={20} />
+          ) : (
+            <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
+          )}
         </button>
       </form>
-
     </div>
-  )
+  );
 }
 
-export default FormPostagem
+export default FormPostagem;
